97b57d035db0c352368092c87db65894111b029d
Globe-Eater <bullokl@ostatemail.okstate.edu>
2019-10-23 16:22:29 -0500

    Adapts to input columns

--- uniques.py
+++ uniques.py
@@ -2,9 +2,6 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_excel('Oklahoma.xls')
-# There's a lot more columns to drop..... like vicnity 
-#df = df.drop(columns=['PROPNAME','RESNAME','ADDRESS','Lat','Long','duplicate_check'])
-df = df[['HIST_FUNC','CURR_FUNC','AREASG_1','AREASG_2']]
 
 def uniques(df):
 	'''I am using this method so I do not have to write df.column_name.uniques
@@ -16,26 +13,35 @@ def uniques(df):
 		uniques(df)
 		returns a dataframe with column names and unique values under each
 		column.
-
+	
+	Dev Notes:
 	.... okay maybe this shouldn't be done on an entire dataset.... 
 	Since we have PROPNAME and ADDRESS that are all unique...
+	Likely will throw some test cases in later to help prevent this from happening.
 	'''
 	
 	names = []
 	for col in df.columns:
-		print(col)
 		names.append(col)
 
 	uniques = []
+	shorten_cols = []
 	for i in names:
 		x = df[i].unique()
-		uniques.append(x)
-	
-	df = pd.DataFrame(uniques,index=names)
+		if len(x) > 50:
+			pass
+		else:
+			uniques.append(x)
+			shorten_cols.append(i)
+	#Okay I have a good list to work off of, but I dropped all some columns...
+	df = pd.DataFrame(uniques,index=shorten_cols)
 	df = df.transpose()
-	print(df)
+	return df
 
-uniques(df)
+# I am pretty sure this is what I wamt. To get more results increase the len(x) > value.
+# Next step is to find out the other No Data type values. such as 00, none, 
+#uniques(df)
+
 
 #if __name__ == '__main__':
 #	run()
