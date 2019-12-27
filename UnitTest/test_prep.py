c55ec4be01e575423fb05718ded6bdcada83ad93
Globe-Eater <bullokl@ostatemail.okstate.edu>
2019-12-26 21:12:42 -0600

    Adding a folder to contain all the unittests to be run on this project.

---
+++ UnitTest/test_prep.py
@@ -0,0 +1,24 @@
+import unittest
+import prep
+
+class Testprep(unittest.TestCase):
+    '''This testing python program is made for prep.py to ensure all train-test-split methods are working as intended
+    and to also make sure all preprocessing is functioning as intended.'''
+
+def setup(self):
+    pass
+
+def teardown(self):
+    pass
+
+def test_fill_empty(self):
+    pass
+
+def test_checknumbers(self):
+    pass
+
+def test_prep(self):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
