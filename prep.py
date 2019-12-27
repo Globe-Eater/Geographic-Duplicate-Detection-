8e4885d5f3380c334a17673b88bfe867bdd822af
Globe-Eater <bullokl@ostatemail.okstate.edu>
2019-12-26 21:08:38 -0600

    Prep.py is for preparing data, 1. Reducing dimensions of the data 2. check to see if the data types and ranges are correct 3. Be abstract so it can be applied to all counites in the dataset.

--- prep.py
+++ prep.py
@@ -4,45 +4,42 @@ import numpy as np
 from scipy import hstack
 import re
 from sklearn.model_selection import train_test_split
-# For testing purposes will be removed afterward
-from paths import pathname
-
-#test line for easy running
-df = pd.read_csv(pathname())
+import re
+import pandas as pd
 
 def fill_empty(df):
-	'''fill_empty takes the arugment df (dataframe)
-	This should be used with apply.
-	For example:
-		df = fill_empty(df)
-	'''
-	df = df.replace(r'^\s*$', np.nan, regex=True)
-	df = df.fillna(Filled_value="No Data")
-	return df
+    '''fill_empty takes the arugment df (dataframe)
+    This should be used with apply.
+    For example:
+        df = fill_empty(df)
+    '''
+    df = df.replace(r'^\s*$', np.nan, regex=True)
+    df = df.fillna(Filled_value="No Data")
+    return df
+
+def checkNumbers(lat, long):
+    '''This method is to make sure that the lats and longs are within the state of Oklahoma.
+    inputs: df['Lat', 'long']]
+    output "Everything should be within Oklahoma.
+    output There is a value that is outside the range of Oklahoma.'''
+    # assert # are the numbers within Oklahoma? Need to look this up and impliment it.
+    pass
 
 def prep(df):
-	'''prep is used for vectorizing the data 
-	so that it can be used in a machine learning model.
-	
-	Dev Notes:
-	Order of OPS:
-		Convert fields like duplicate_check from text to
-	1 or 0. 
-
-		Train Test split. 
-	'''
-	train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-	
-
-	def ngrams(string, n=3):
-		'''This method both cleans special characters
-		out of the strings and prepares themm for being
-		proccessed by the TF-IDF'''
-		string = re.sub(r',-./&',r'', string)
-		ngrams = zip(*[string[i:] for i in range(n)])
-		return [''.join(ngram) for ngram in ngrams]
-	
-	
-	#for x in X_train:
-	#Do ngrams things
-prep(df)
+    '''prep is used for vectorizing the data 
+    so that it can be used in a machine learning model.
+    Dev Notes:
+    Order of OPS:
+    Convert fields like duplicate_check from text to
+    1 or 0. 
+    Train Test split. 
+    '''
+    df = df[['OBJECTID', 'ADDRESS', 'RESNAME', 'Lat', 'Long', 'duplicate_check']]
+    def labels(duplicate_check):
+        '''This method is to be applied to the dataframe df, that has a column duplicate_check in it.
+        This is being used to convert poss_dup to 1 or good to 0. Later this will be expanded to cover anything else
+        within the dataset.'''
+        if duplicate_check == 'good':
+            return 0
+        else:
+            return 1
