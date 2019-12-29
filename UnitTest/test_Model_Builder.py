aac6858c57c6c2004127215fc6fad7ebec7d6038
Globe-Eater <bullokl@ostatemail.okstate.edu>
2019-12-28 19:55:43 -0600

    Started test_Model_Builder.py, I am doing this becuase I am having issues with the vectorizer function in Model_Builder.py. I just setup the skeleton for the file I will implement test at a later time.

---
+++ UnitTest/test_Model_Builder.py
@@ -0,0 +1,21 @@
+import unittest
+from Model_Builder import prepocess
+
+
+class Test_Model_Builder(unittest.TestCase):
+    '''This class is to test Model_Builder to ensure the machine learning framework works correctly.'''
+
+    def setup():
+        pass
+
+    def teardown():
+        pass
+
+    def test_preprocess():
+        pass
+
+    def test_vectorizer():
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
