3f9b8b8cbb1f8b0db398378ec8f600be4a08b2eb
Globe-Eater <bullokl@ostatemail.okstate.edu>
2020-01-04 11:02:04 -0600

    Added some quality of life pandas functions to combine the predicted results with the input data. This was done so you can see records that are duplicates and see how confident it is and if its close.

--- Classifier.py
+++ Classifier.py
@@ -4,47 +4,49 @@ from prep import start
 from Model_Builder import preprocess
 
 
-def main():
-    '''This program will predict duplicate records that are input into it.
-    Requires: prepared data from prep.py.
-    
-    Output: an excel file of objectID and prediction.
-    '''
-    
-    df = start()
-    
-    # Handle Object ID,
-    Eval_ObjectID = df['OBJECTID']
-    
-    # Preprocess the data:
-    data = preprocess(df)
-    
-    # Shapes of Features (independent variables) and Labels (dependent variables)
-    label_count = df['duplicate_check'].shape[0]
-    
-    # Construct Variables This needs to be done for the restored Model.
-    X = tf.placeholder(tf.float32, shape=(None, label_count), name="X")
-    
-    # To make the shapes aline
-    data = data.transpose()
-    
-    # Reinitialize the new variables:
-    init = tf.global_variables_initializer()
-    
-    # This loads the graph structure
-    saver = tf.train.import_meta_graph("saved_models/model_final.ckpt.meta")
-    
-    # Loads in accuracy Measurement:
-    theta = tf.get_default_graph().get_tensor_by_name('theta:0')
-    
-    with tf.Session() as sess:
-        saver.restore(sess, "saved_models/model_final.ckpt")
-        init.run()
-        X_new_data = data
-        prediction = theta.eval(feed_dict={X: X_new_data})
-    
-    output = pd.DataFrame(pd.np.column_stack([Eval_ObjectID, prediction]))
-    output.to_excel('Prediction.xlsx')
+#def main():
+'''This program will predict duplicate records that are input into it.
+Requires: prepared data from prep.py.
 
-if __name__ == '__main__':
-    main()
+Output: an excel file of objectID and prediction.
+'''
+
+df = start()
+
+# Handle Object ID,
+Eval_ObjectID = df['OBJECTID']
+
+# Preprocess the data:
+data = preprocess(df)
+
+# Shapes of Features (independent variables) and Labels (dependent variables)
+label_count = df['duplicate_check'].shape[0]
+
+# Construct Variables This needs to be done for the restored Model.
+X = tf.placeholder(tf.float32, shape=(None, label_count), name="X")
+
+# To make the shapes aline
+data = data.transpose()
+
+# Reinitialize the new variables:
+init = tf.global_variables_initializer()
+
+# This loads the graph structure
+saver = tf.train.import_meta_graph("saved_models/model_final.ckpt.meta")
+
+# Loads in accuracy Measurement:
+theta = tf.get_default_graph().get_tensor_by_name('theta:0')
+
+with tf.Session() as sess:
+    saver.restore(sess, "saved_models/model_final.ckpt")
+    init.run()
+    prediction = theta.eval(feed_dict={X: data})
+
+output = pd.DataFrame(pd.np.column_stack([Eval_ObjectID, prediction]))
+output = output.rename(columns=({0: 'OBJECTID', 1: 'Prediction'}))
+df.merge(output, on=['OBJECTID'], how='left')
+df.sort_values(by=['duplicate_check'], ascending=False)
+df.to_excel('Prediction.xlsx')
+
+#if __name__ == '__main__':
+#    main()
